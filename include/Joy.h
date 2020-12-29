23eadc787eb5c19690f36e6e361fb57cde465561
Jaime Pavlich-Mariscal <jaime.pavlich@gmail.com>
2020-12-29 13:58:03 -0500

    .

--- include/Joy.h
+++ include/Joy.h
@@ -4,12 +4,9 @@
 #include <vector>
 #include "AnalogValue.h"
 #include <cmath>
-#include  "common.h"
+#include "common.h"
 
 const auto DATA_SIZE = sizeof(JoystickReport_t) - 1;
-
-// -16852
-// 10383
 
 const float THROTTLE_MIN = (32767 - 16852) >> 4;
 const float THROTTLE_MAX = (32767 + 10383) >> 4;
@@ -18,47 +15,17 @@ class Joy : public HIDJoystick
 {
 private:
 public:
-    std::vector<AnalogValue> values = {
-        AnalogValue(PA0),
-        AnalogValue(PA1),
-        AnalogValue(PA2),
-        AnalogValue(PA3),
-        AnalogValue(PA4, THROTTLE_MIN, THROTTLE_MAX),
-        AnalogValue(PA5),
-    };
-    std::vector<int> buttons = {
-        // DISABLED,
-        // DISABLED,       
-        PB7,        
-        // PB6,        
-        // PB5,        
-        // PB4,        
-        // PA15,       
-        // PA10,       
-        // PA9,        
-        // PA8,        
-        // PB15,       
-        // PB14,       
-        // PB13,
-        // PB12,
-        // PB11,
-        // PB10,
-        // PB1,
-        // PA7,
-        // PA6,
-        // PA5,
-        // PB9,
-        // PB8,
-    };
+    std::vector<AnalogValue> axes;
+    std::vector<int> button_pins;
 
-    Joy(USBHID &_HID)
-        : HIDJoystick(_HID)
+    Joy(USBHID &_HID, const std::vector<AnalogValue>& axes, const std::vector<int>& button_pins)
+        : HIDJoystick(_HID), axes(axes), button_pins(button_pins)
     {
     }
 
     void setup()
     {
-        for (auto pin : buttons)
+        for (auto pin : button_pins)
         {
             pinMode(pin, INPUT_PULLUP);
         }
@@ -71,21 +38,21 @@ public:
 
     void loop()
     {
-        auto s = (1023 - values[5].get()) / 1023;
+        auto s = (1023 - axes[5].get()) / 1023;
 
-        joyReport.x = scale(1023 - values[0].get(), s);
-        joyReport.y = scale(values[1].get(), s);
-        joyReport.rx = scale(1023 - values[2].get(), s);
-        joyReport.ry = scale(values[3].get(), s);
+        joyReport.x = scale(1023 - axes[0].get(), s);
+        joyReport.y = scale(axes[1].get(), s);
+        joyReport.rx = scale(1023 - axes[2].get(), s);
+        joyReport.ry = scale(axes[3].get(), s);
 
-        joyReport.sliderLeft = 1023 - values[4].get();
+        joyReport.sliderLeft = 1023 - axes[4].get();
 
         joyReport.buttons = 0;
-        for (size_t i = 0; i < buttons.size(); i++)
+        for (size_t i = 0; i < button_pins.size(); i++)
         {
-            if (buttons[i] != DISABLED)
+            if (button_pins[i] != DISABLED)
             {
-                joyReport.buttons |= (0x1 & ~digitalRead(buttons[i])) << i;
+                joyReport.buttons |= (0x1 & ~digitalRead(button_pins[i])) << i;
             }
         }
 
